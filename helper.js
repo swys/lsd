d35cff9b6fd664e9d4170fa13d4a81e91c50651e
swys <abacus822@gmail.com>
2013-10-13 10:41:34 -0400

    added helper file the move some functions out of main class file and updated lsd.js to use the helper module.

---
+++ helper.js
@@ -0,0 +1,45 @@
+function gotDir(err, contents) {
+    var that = this;
+    if (err) {
+        this.emit('error', err);
+        this.writeNext();
+        this.done();
+    } else {
+        this.emit('enter', this._current);
+        if (contents.length === 0) {
+            this.emit('empty', this._current);
+            this.writeNext();
+            this.done();
+        } else {
+            this._count = contents.length;
+            contents.forEach(function(file) {
+                var fullPath = that._current + path.sep + file;
+                that._stat(fullPath, statCB.bind(that));
+            });
+        }
+    }
+}
+
+function statCB() {
+    if (this._count === 0) {
+        this.emit('exit', this._current);
+        this.writeNext();
+        this.done();
+    }
+}
+
+// Returns true only if test value is equals true...will return false for anything else
+// Example isBool(5) --> false isBool() --> false isBool(isBool(isBool(0))) --> false isBool(true) --> true
+function isBool(test) {
+    if (test === true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {
+    gotDir : gotDir,
+    statCB : statCB,
+    isBool : isBool
+};
--- lsd.js
+++ lsd.js
@@ -1,13 +1,14 @@
 var Transform = require('stream').Transform,
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    helper = require('./helper.js');
 
 function Lsd(opts) {
     if (!(this instanceof Lsd)) {
         return new Lsd(opts);
     }
     if (opts) {
-        this.recurse = isBool(opts.recurse);
+        this.recurse = helper.isBool(opts.recurse);
     } else {
         this.recurse = false;
     }
@@ -27,37 +28,38 @@ Lsd.prototype = Object.create(Transform.prototype, {
 Lsd.prototype._transform = function(root, encoding, done) {
     this._current = path.resolve(root);
     this.done = done;
-    fs.readdir(this._current, gotDir.bind(this));
+    fs.readdir(this._current, helper.gotDir.bind(this));
 };
 
 Lsd.prototype._stat = function(item, cb) {
+    var that = this;
     fs.stat(item, function(err, stats) {
         if (err) {
-            this._count -= 1;
-            this.emit('error', err);
-            this.push(item);
+            that._count -= 1;
+            that.emit('error', err);
+            that.push(item);
             cb();
         } else {
             if (stats.isDirectory()) {
-                this._count -= 1;
-                if (this.recurse === true) {
-                    this._writeQ.push(item);
+                that._count -= 1;
+                if (that.recurse === true) {
+                    that._writeQ.push(item);
                 }
-                this.emit('directory', item);
-                this.push(item);
+                that.emit('directory', item);
+                that.push(item);
                 cb();
             } else if (stats.isFile()) {
-                 this._count -= 1;
-                 this.emit('file', item);
-                 this.push(item);
+                 that._count -= 1;
+                 that.emit('file', item);
+                 that.push(item);
                  cb();
            } else {
-               this._count -= 1;
-               this.emit('notFileorDir', item);
+               that._count -= 1;
+               that.emit('notFileorDir', item);
                cb();
            }
         }
-    }.bind(this));
+    };
 };
 
 Lsd.prototype.writeNext = function() {
@@ -71,46 +73,6 @@ Lsd.prototype.writeNext = function() {
         return;
     }
 };
-
-function gotDir(err, contents) {
-    var that = this;
-    if (err) {
-        this.emit('error', err);
-        this.writeNext();
-        this.done();
-    } else {
-        this.emit('enter', this._current);
-        if (contents.length === 0) {
-            this.emit('empty', this._current);
-            this.writeNext();
-            this.done();
-        } else {
-            this._count = contents.length;
-            contents.forEach(function(file) {
-                var fullPath = that._current + path.sep + file;
-                that._stat(fullPath, statCB.bind(that));
-            });
-        }
-    }
-}
-
-function statCB() {
-    if (this._count === 0) {
-        this.emit('exit', this._current);
-        this.writeNext();
-        this.done();
-    }
-}
-
-// Returns true only if test value is equals true...will return false for anything else
-// Example isBool(5) --> false isBool() --> false isBool(isBool(isBool(0))) --> false isBool(true) --> true
-function isBool(test) {
-    if (test === true) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
 module.exports = function(opts) {
     return new Lsd(opts);
